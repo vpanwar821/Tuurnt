bfdb927bfd7be688ec6a3579949ab67692b55b10
Aniket <aniketengg.225@gmail.com>
2018-02-22 01:54:13 +0530

    Token & Vesting Added

---
+++ contracts/TurrntToken.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.4.18;
+
+import './helpers/StandardToken.sol';
+import './lib/SafeMath.sol';
+
+contract TurrntToken is StandardToken {
+
+    using SafeMath for uint256;
+
+    // token Variable declaration
+    string public name = "Tuurnt Token";
+    string public symbol = "TRT";
+    uint16 public decimals = 18;
+    uint256 public totalSupply = 500000000 * 10 ** 18;
+
+    // distribution variables
+    uint256 public tokenAllocToTeam;
+    uint256 public tokenAllocToCrowdsale;
+    uint256 public tokenAllocToCompany;
+    uint256 public allocatedTokens;
+
+    // addresses
+    address public owner;
+    address public crowdsaleAddress;
+    address public vestingContractAddress;
+    address public companyAddress;
+
+    function TurrntToken(address _crowdsaleAddress, address _vestingContract, address _companyAddress) public {
+        tokenAllocToTeam = ((totalSupply.mul(33)).div(100)) * 10 ** 18;     // 33 % Allocation
+        tokenAllocToCompany = ((totalSupply.mul(33)).div(100)) * 10 ** 18;  // 33 % Allocation 
+        tokenAllocToCrowdsale = ((totalSupply.mul(34)).div(100)) * 10 ** 18;// 34 % Allocation
+
+        // Address 
+        owner = msg.sender;        
+        crowdsaleAddress = _crowdsaleAddress;
+        vestingContractAddress = _vestingContract;
+        companyAddress = _companyAddress;
+
+        // Allocations
+        balances[crowdsaleAddress] = tokenAllocToCrowdsale;
+        balances[companyAddress] = tokenAllocToCompany;
+        balances[vestingContractAddress] = tokenAllocToTeam;
+
+        allocatedTokens = balances[companyAddress];
+    }  
+
+    function transferRemainingToCompany() public returns(bool) {
+        require(msg.sender == crowdsaleAddress);
+        uint256 remainingTokens = balances[crowdsaleAddress];
+        totalSupply = totalSupply.sub(remainingTokens);
+        balances[crowdsaleAddress] = 0;
+        balances[companyAddress].add(remainingTokens); 
+        return true;
+    }
+
+
+}
---
+++ contracts/VestingStrategy.sol
@@ -0,0 +1,104 @@
+ pragma solidity ^0.4.18;
+
+import './lib/SafeMath.sol';
+import './TurrntToken.sol';
+
+contract VestingStrategy {
+
+    using SafeMath for uint256;
+
+    TurrntToken token;
+    // Variable declaration
+    address public founderAddress;
+    address public teamAddress;
+    address public tokenAddress;
+
+    uint256 public firstSlotTimestamp;
+    uint256 public secondSlotTimestamp;
+    uint256 public thirdSlotTimestamp;
+    uint256 public finalSlotTimestamp;
+    uint256 public vestingPeriod;
+    uint256 public tokenReleased = 0;
+    uint256 public slotAmount = 41250000 * 10 ** 18; //25% of total team token
+
+    bool private isTokenSet = false;
+
+    event OwnershipTransferred(uint256 _timestamp, address _newFounderAddress);
+
+    modifier onlyFounder(){
+        require(msg.sender == founderAddress);
+        _;
+    }
+
+    function VestingStrategy(address _teamAddress) public {
+        teamAddress = _teamAddress;
+        founderAddress = msg.sender;
+        firstSlotTimestamp = now + 1 * 365 days;
+        secondSlotTimestamp = firstSlotTimestamp + 1 * 365 days;
+        thirdSlotTimestamp = secondSlotTimestamp + 1 * 365 days;
+        finalSlotTimestamp = thirdSlotTimestamp + 1 * 365 days; // To cover the month days difference
+        vestingPeriod = now + 4 * 365 days;   // 3 months for crowdsale end + 2 years of vesting
+    }
+
+    function setTokenAddress(address _tokenAddress) onlyFounder public returns (bool) {
+        require(_tokenAddress != address(0));
+        require(isTokenSet == false);
+        token = TurrntToken(_tokenAddress);
+        tokenAddress = _tokenAddress;
+        isTokenSet = !isTokenSet;
+        return true;
+    }
+
+    function transferOwnership(address _newFounderAddress) onlyFounder public returns(bool) {
+        founderAddress = _newFounderAddress;
+        OwnershipTransferred(now,_newFounderAddress);
+        return true;
+    }
+
+    function releaseTokenToTeam() onlyFounder public returns(bool) {
+        require(isTokenSet == true);
+        if (now >= finalSlotTimestamp) {
+            if (tokenReleased == 0) {
+                require(token.transfer(teamAddress, 4*slotAmount));
+                tokenReleased = tokenReleased.add(4*slotAmount);
+            } else if ((tokenReleased).div(slotAmount) == 1) {
+                require(token.transfer(teamAddress, 3*slotAmount));
+                tokenReleased = tokenReleased.add(3*slotAmount);
+            } else if ((tokenReleased).div(slotAmount) == 2) {
+                require(token.transfer(teamAddress, 2*slotAmount));
+                tokenReleased = tokenReleased.add(2*slotAmount);
+            } else if ((tokenReleased).div(slotAmount) == 3) {
+                require(token.transfer(teamAddress, slotAmount));
+                tokenReleased = tokenReleased.add(slotAmount);
+            } 
+        }else if (now >= thirdSlotTimestamp) {
+            if (tokenReleased == 0) {
+                require(token.transfer(teamAddress, 3*slotAmount));
+                tokenReleased = tokenReleased.add(3*slotAmount);
+            } else if ((tokenReleased).div(slotAmount) == 1) {
+                require(token.transfer(teamAddress, 2*slotAmount));
+                tokenReleased = tokenReleased.add(2*slotAmount);
+            } else if ((tokenReleased).div(slotAmount) == 2) {
+                require(token.transfer(teamAddress, slotAmount));
+                tokenReleased = tokenReleased.add(slotAmount);
+            }                
+        }else if (now >= secondSlotTimestamp) {
+            if (tokenReleased == 0) {
+                require(token.transfer(teamAddress, 2*slotAmount));
+                tokenReleased = tokenReleased.add(2*slotAmount);
+            } else {
+                require(token.transfer(teamAddress, slotAmount));
+                tokenReleased = tokenReleased.add(slotAmount);
+            }                 
+        }else if (now >= firstSlotTimestamp) {
+            require(token.transfer(teamAddress, slotAmount));
+            tokenReleased = tokenReleased.add(slotAmount);
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    
+
+}
