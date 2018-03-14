4229dd11e6a968677b29d80a9bb13403df9f0c56
Aniket <aniketengg.225@gmail.com>
2018-03-14 17:33:59 +0530

    contracts merged

---
+++ flat/TuurntCrowdsale.sol
@@ -0,0 +1,531 @@
+pragma solidity ^0.4.18;
+
+/**
+ * @title Math
+ * @dev Assorted math operations
+ */
+library Math {
+  function max64(uint64 a, uint64 b) internal pure returns (uint64) {
+    return a >= b ? a : b;
+  }
+
+  function min64(uint64 a, uint64 b) internal pure returns (uint64) {
+    return a < b ? a : b;
+  }
+
+  function max256(uint256 a, uint256 b) internal pure returns (uint256) {
+    return a >= b ? a : b;
+  }
+
+  function min256(uint256 a, uint256 b) internal pure returns (uint256) {
+    return a < b ? a : b;
+  }
+}
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+
+  uint256 totalSupply_;
+
+  /**
+  * @dev total number of tokens in existence
+  */
+  function totalSupply() public view returns (uint256) {
+    return totalSupply_;
+  }
+
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
+
+    // SafeMath.sub will throw if there is not enough balance.
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) internal allowed;
+
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
+
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   *
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+
+  /**
+   * @dev Increase the amount of tokens that an owner allowed to a spender.
+   *
+   * approve should be called when allowed[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param _spender The address which will spend the funds.
+   * @param _addedValue The amount of tokens to increase the allowance by.
+   */
+  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+  /**
+   * @dev Decrease the amount of tokens that an owner allowed to a spender.
+   *
+   * approve should be called when allowed[_spender] == 0. To decrement
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param _spender The address which will spend the funds.
+   * @param _subtractedValue The amount of tokens to decrease the allowance by.
+   */
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+}
+
+contract DetailedERC20 is ERC20 {
+  string public name;
+  string public symbol;
+  uint8 public decimals;
+
+  function DetailedERC20(string _name, string _symbol, uint8 _decimals) public {
+    name = _name;
+    symbol = _symbol;
+    decimals = _decimals;
+  }
+}
+
+/**
+* @title TuurntToken 
+* @dev The TuurntToken contract contains the information about 
+* Tuurnt token.
+*/
+
+
+
+
+
+contract TuurntToken is StandardToken, DetailedERC20 {
+
+    using SafeMath for uint256;
+
+    // distribution variables
+    uint256 public tokenAllocToTeam;
+    uint256 public tokenAllocToCrowdsale;
+    uint256 public tokenAllocToCompany;
+    uint256 public remainingTokens;
+
+    // addresses
+    address public owner;
+    address public crowdsaleAddress;
+    address public vestingContractAddress;
+    address public companyAddress;
+   
+
+    /**
+    * @dev The TuurntToken constructor set the orginal crowdsaleAddress,vestingAddress and companyAddress and allocate the
+    * tokens to them.
+    * @param _crowdsaleAddress The address of crowsale contract
+    * @param _vestingContract The address of vesting contract
+    * @param _companyAddress The address of company address 
+    */
+
+    function TuurntToken(address _crowdsaleAddress, address _vestingContract, address _companyAddress, string _name, string _symbol, uint8 _decimals) public 
+        DetailedERC20(_name, _symbol, _decimals)
+    {
+        require(_crowdsaleAddress != address(0));
+        require(_vestingContract != address(0));
+        require(_companyAddress != address(0));
+        totalSupply_ = 500000000 * 10 ** 18;
+        _name = "Tuurnt Token";
+        _symbol = "TRT";
+        _decimals = 18; 
+        tokenAllocToTeam = (totalSupply_.mul(33)).div(100);     // 33 % Allocation
+        tokenAllocToCompany = (totalSupply_.mul(33)).div(100);  // 33 % Allocation 
+        tokenAllocToCrowdsale = (totalSupply_.mul(34)).div(100);// 34 % Allocation
+
+        // Address 
+        owner = msg.sender;        
+        crowdsaleAddress = _crowdsaleAddress;
+        vestingContractAddress = _vestingContract;
+        companyAddress = _companyAddress;
+        
+
+
+        // Allocations
+        balances[crowdsaleAddress] = tokenAllocToCrowdsale;
+        balances[companyAddress] = tokenAllocToCompany;
+        balances[vestingContractAddress] = tokenAllocToTeam; 
+
+        //transfer event
+        Transfer(address(0), crowdsaleAddress, tokenAllocToCrowdsale);
+        Transfer(address(0), companyAddress, tokenAllocToCompany);
+        Transfer(address(0), vestingContractAddress, tokenAllocToTeam);
+        
+    }  
+}
+
+/**
+* @title TuurntCrowdsale
+* @dev The Crowdsale contract holds the token for the public sale of token and 
+* contains the function to buy token.  
+*/
+
+
+
+
+
+
+contract TuurntCrowdsale is Ownable {
+
+    using SafeMath for uint256;
+
+    TuurntToken public token;
+
+    //variable declaration
+    uint256 public MIN_INVESTMENT = 0.2 ether;
+    uint256 public MAX_INVESTMENT = 10 ether;
+    uint256 public ethRaised;
+    uint256 public ethRate = 86300;
+    uint256 public startCrowdsaleDate;
+    uint256 public endCrowdsaleDate;
+    uint256 public startPresaleDate;
+    uint256 public endPresaleDate;
+    uint256 public soldToken = 0;
+    uint256 public amount;
+
+    //addresses
+    address public beneficiaryAddress;
+    address public tokenAddress;
+
+    event TokenBought(address indexed _investor, uint256 _token, uint256 _timestamp);
+    event LogTokenSet(address _token, uint256 _timestamp);
+    enum State {PreSale, CrowdSale, Finish}
+
+    /**
+    * @dev Transfer the ether to the beneficiaryAddress.
+    * @param _fund The ether that is transferred to contract to buy tokens.  
+    */
+    function fundTransfer(uint256 _fund) internal returns(bool) {
+        beneficiaryAddress.transfer(_fund);
+        return true;
+    }
+
+    /**
+    * @dev fallback function which accepts the ether and call the buy token function.
+    */
+    function () payable public {
+        buyTokens(msg.sender);
+    }
+
+    /**
+    * @dev TuurntCrowdsale constructor sets the original beneficiaryAddress and 
+    * set the timeslot for the Pre-ICO and ICO.
+    * @param _beneficiaryAddress The address to transfer the ether that is raised during crowdsale. 
+    */
+    function TuurntCrowdsale(address _beneficiaryAddress) public {
+        require(_beneficiaryAddress != address(0));
+        beneficiaryAddress = _beneficiaryAddress;
+        startPresaleDate = now;
+        endPresaleDate = now + 2 days;
+        startCrowdsaleDate = endPresaleDate; 
+        endCrowdsaleDate = startCrowdsaleDate + 6 weeks;
+    }
+
+    /**
+    * @dev Allow founder to set the token contract address.
+    * @param _tokenAddress The address of token contract.
+    */
+    function setTokenAddress(address _tokenAddress) onlyOwner public returns(bool) {
+        require(tokenAddress == address(0));
+        token = TuurntToken(_tokenAddress);
+        tokenAddress = _tokenAddress;
+        LogTokenSet(token, now);
+        return true;
+    }
+
+    /**
+    * @dev Allow founder to change the minimum investment of ether.
+    * @param _newMinInvestment The value of new minimum ether investment. 
+    */
+    function changeMinInvestment(uint256 _newMinInvestment) onlyOwner public {
+        MIN_INVESTMENT = _newMinInvestment;
+    }
+
+    /**
+    * @dev Allow founder to change the maximum investment of ether.
+    * @param _newMaxInvestment The value of new maximum ether investment. 
+    */
+    function changeMaxInvestment(uint256 _newMaxInvestment) onlyOwner public {
+        MAX_INVESTMENT = _newMaxInvestment;
+    }
+
+    /**
+    * @dev Return the state based on the timestamp. 
+    */
+    function getState() view public returns(State) {
+
+        if (now >= startPresaleDate && now <= endPresaleDate) {
+            return State.PreSale;
+        } 
+        if (now >= startCrowdsaleDate && now <= endCrowdsaleDate) {
+            return State.CrowdSale;
+        } else {
+            return State.Finish;
+        }
+
+    }
+
+    /**
+    * @dev Return the rate based on the state and timestamp.
+    */
+    function getRate() view public returns(uint256) { 
+        if (getState() == State.PreSale) {
+            return 6;
+        }
+        if (getState() == State.CrowdSale) {
+            if (now >= startCrowdsaleDate + 2 weeks && now <= endCrowdsaleDate) {
+                return 10;
+            }
+            if (now >= startCrowdsaleDate + 1 weeks) {
+                return 8;
+            }
+            if (now >= startCrowdsaleDate) {
+                return 7;
+            }
+            return 0;
+        }
+    } 
+
+    /**
+    * @dev Calculate the number of tokens to be transferred to the investor address 
+    * based on the invested ethers.
+    * @param _investedAmount The value of ether that is invested.  
+    */
+    function getTokenAmount(uint256 _investedAmount) view public returns(uint256) {
+        uint256 tokenRate = getRate();
+        uint256 tokenAmount;
+        tokenAmount = _investedAmount.mul((ethRate.div(tokenRate)));
+        return tokenAmount;
+    }
+
+    /**
+    * @dev Transfer the tokens to the investor address.
+    * @param _investorAddress The address of investor. 
+    */
+    function buyTokens(address _investorAddress) 
+    public 
+    payable
+    returns(bool)
+    {
+        require(_investorAddress != address(0));
+        require(tokenAddress != address(0));
+        require(msg.value >= MIN_INVESTMENT && msg.value <= MAX_INVESTMENT);
+        amount = getTokenAmount(msg.value);
+        require(fundTransfer(msg.value));
+        require(token.transfer(_investorAddress, amount));
+        ethRaised = ethRaised.add(msg.value);
+        soldToken = soldToken.add(amount);
+        TokenBought(_investorAddress,amount,now);
+        return true;
+    }
+
+    /**
+    * @dev Allow founder to end the crowsale and transfer the remaining
+    * tokens of crowdfund to the company address. 
+    */
+    function endCrowdfund(address companyAddress) onlyOwner public returns(bool) {
+        require(now > endCrowdsaleDate);
+        uint256 remaining = token.balanceOf(this);
+        require(token.transfer(companyAddress, remaining));
+    }
+
+}
